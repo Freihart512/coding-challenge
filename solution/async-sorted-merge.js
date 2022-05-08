1fcc52000a5a24c8c4fa2530749914302c7dc290
Freihart <carlosfabian512@gmail.com>
2022-05-08 15:25:04 -0500

    Async Solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,23 @@
 "use strict";
+const _ = require("lodash");
 
 // Print all entries, across all of the *async* sources, in chronological order.
-
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    while (logSources.length > 0) {
+      logSources.sort((a, b) => a.last.date - b.last.date)
+      const nextSourceToPrint = logSources[0]
+      printer.print(nextSourceToPrint.last)
+      try {
+        if (await nextSourceToPrint.popAsync() === false) {
+          logSources.shift()
+        }
+      } catch (error) {
+        reject(error)
+      }
+    }
+
+    printer.done();
     resolve(console.log("Async sort complete."));
-  });
+  })
 };
