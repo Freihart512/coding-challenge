5c57415320adcd652217c337c51f1ce5422740ee
Freihart <carlosfabian512@gmail.com>
2022-05-08 15:25:40 -0500

    Sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,17 @@
 "use strict";
-
+const _ = require("lodash");
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  while (logSources.length > 0) {
+    logSources.sort((a, b) => a.last.date - b.last.date)
+    const nextToPrint = logSources[0]
+    printer.print(nextToPrint.last)
+    nextToPrint.pop()
+    if (nextToPrint.drained) {
+      logSources.shift()
+    }
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
